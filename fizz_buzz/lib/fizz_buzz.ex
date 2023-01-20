9d241f9e509268bc532a042232b7fd0193e9a47b
minccia <lusyva.pro@gmail.com>
2023-01-20 00:07:46 -0300

    Implementa modulo FizzBuzz

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -1,18 +1,27 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
+  defp fizz_buzz(num) when (rem(num, 3) == 0) and (rem(num, 5) == 0), do: "FizzBuzz"
+  defp fizz_buzz(num) when (rem(num, 3) == 0), do: "Fizz"
+  defp fizz_buzz(num) when (rem(num, 5) == 0), do: "Buzz"
+  defp fizz_buzz(num), do: num
 
-  @doc """
-  Hello world.
+  defp handle_number(num) do
+    num
+    |> String.to_integer()
+    |> fizz_buzz()
+  end
 
-  ## Examples
+  defp parse_list(numbers) do
+    numbers
+    |> String.split(",")
+    |> Enum.map(&handle_number/1)
+  end
 
-      iex> FizzBuzz.hello()
-      :world
+  defp handle_file({:ok, result}), do: {:ok, parse_list(result)}
+  defp handle_file({:error, message}), do: {:error, "Deu ruim: #{message}"}
 
-  """
-  def hello do
-    :world
+  def build(file_path) do
+    file_path
+    |> File.read()
+    |> handle_file()
   end
 end
